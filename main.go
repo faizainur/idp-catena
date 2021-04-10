f3755e05858ee6559df58df9a348348ae7703afd
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-04-11 00:56:54 +0700

    fix cors

--- main.go
+++ main.go
@@ -140,10 +140,10 @@ func setupRedis() *redis.Client {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*, catena.id, dashboard.catena.id, api.catena.id")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, PUT")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST")
 
 		// if c.Request.Method == "OPTIONS" {
 		// 	c.AbortWithStatus(204)
