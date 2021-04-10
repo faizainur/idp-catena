dffde32608af38b6575b6b35d202ded7756426aa
Faiz Ainur Rofiq <cavadevop@gmail.com>
2021-04-11 01:48:25 +0700

    fix cors

--- main.go
+++ main.go
@@ -141,9 +141,22 @@ func setupRedis() *redis.Client {
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST")
+
+		origin := c.Request.Header.Get("Origin")
+		fmt.Println(origin)
+
+		if origin == "https://dashboard.catena.id" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "dashboard.catena.id")
+		} else if origin == "https://api.catena.id" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "api.catena.id")
+		} else if origin == "https://catena.id" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "catena.id")
+		} else {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		}
 
 		// if c.Request.Method == "OPTIONS" {
 		// 	c.AbortWithStatus(204)
